c47e38140ea33204bad6c644dcdf6467757a1cb7
lisadeng <lisadeng@mit.edu>
2017-11-15 20:38:12 -0500

    currently wrestling with some indexing errors into our bits

--- rank_select_correct.c
+++ rank_select_correct.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define logK 4
+#define logK 3
 #define K (1 << logK)
 #define N (2 << K)
 #define logN (K+1)
@@ -23,46 +23,79 @@ void fill_table(){
 
 // return how many one bits are left of this position 
 int rank(int pos, int vector){
+	return 0;
+}
 
+void print_bits(int *bit_string, int len) {
+	for (int i = 0; i < len; i++) {
+		for (int j = 31; j >= 0; j--) {
+			printf("%d", (bit_string[i]>>j)&1);
+		}
+	}
+	printf("\n");
 }
 
 int main(){
-
 	int bitString [N/32] = {0};
+	bitString[0] = (1<<20)+1;
+	bitString[2] = (1<<20)+1;
 
+	// bits in chunk -> logSqN
 	// these chunks can be smaller
 	int chunks [(N/logSqN)+1]; 
-
-	int subchunks[(N/logSqN)+1][K];
+	int subchunk_size = 32;
+	int chunk_size = logSqN;
+	int subchunks[(N/logSqN)+1][32];
+	//int subchunks[64][K];
 	int count = 0;
+	printf("chunk size = %d, subchunk size = %d\n", chunk_size, subchunk_size);
 
 	// filling chunks
 	for(int i=0; i<N; i+=logSqN){
+		
 		int j = i;
 		if(j%32 !=0){
 			int partial = bitString[j/32];
 			partial = partial << (j%32);
 			count += __builtin_popcount(partial);
-			j += ((j/32) + 1) * 32;
+			j += (32-(j%32));
 		}
 
 		while(j+32 <(i+logSqN) && (j<N)){
 			count+=__builtin_popcount(bitString[j/32]);
+			j+=32;
 		}
-		if(j%32 !=0){
+		if(j<(i+logSqN) && (j<N)){
 			int partial = bitString[j/32];
-			partial = partial >> (32-(j%32));
+			partial = partial >> (32-((i+logSqN)%32));
 			count += __builtin_popcount(partial);
+			j+=((i+logSqN)%32);
 		}
 		chunks[i/logSqN] = count;
 	}
 
+	// subchunks
 	for(int x=0; x<(N/logSqN)+1; x++){
-
-		for(int y=0; y<K; y+=loglogN){
-
+		int subchunk_count = 0;
+		printf("new chunk\n");
+		for(int y=0; y<=logSqN/32; y+=1){
+			printf("start subchunk = %d\n", ((x*logSqN+y*32))+((x*logSqN+y*32)%32));
+			printf("end subchunk = %d\n", (x*logSqN+y*32)+32-((x*logSqN+y*32)%32));
+			int first_part = bitString[((x*logSqN+y*32)/32)] << ((x*logSqN+y*32)%32);
+			int second_part = bitString[((x*logSqN+y*32)/32)+1] >> (32-((x*logSqN+y*32)%32));
+			subchunk_count += __builtin_popcount(first_part);
+			subchunk_count += __builtin_popcount(second_part);
+			subchunks[x][y]=subchunk_count;
 		}
 	}
+	print_bits(bitString, N/32);
+	for(int x=0; x<(N/logSqN)+1; x++){
+		printf("chunk size = %d\n", chunks[x]);
+		for(int y=0; y<logSqN/32; y+=1){
+			printf("subchunk size = %d\n",subchunks[x][y]);
+		}
+	}
+
 }
 
 
